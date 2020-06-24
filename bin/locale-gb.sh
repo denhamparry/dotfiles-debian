a51960c81f4b6a80c3a66007f215b0540edb7099
Lewis Denham-Parry <lewis@control-plane.io>
2020-06-24 16:52:11 +0100

    fix: set correct name for function
    - set to locale-gb()

--- bin/locale-gb.sh
+++ bin/locale-gb.sh
@@ -4,7 +4,7 @@
 
 # Message shown when logging onto a box next set to GB locale
 
-update_iwlwifi(){
+locale-gb(){
     echo "LC_ALL=en_GB.UTF-8" >> /etc/environment
     echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
     echo "LANG=en_GB.UTF-8" > /etc/locale.conf
