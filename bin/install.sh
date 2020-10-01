6cfb2e7da61eb090e2390cda750f059fa6c01d61
Lewis Denham-Parry <lewis@control-plane.io>
2020-10-01 17:05:05 +0100

    feat: add 1password
    - because 1passwordX can only go so far

--- bin/install.sh
+++ bin/install.sh
@@ -193,6 +193,11 @@ base() {
 
 	setup_sudo
 
+	# 1password
+	sudo apt-key --keyring /usr/share/keyrings/1password.gpg adv --keyserver keyserver.ubuntu.com --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
+	echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password.gpg] https://onepassword.s3.amazonaws.com/linux/debian edge main' | sudo tee /etc/apt/sources.list.d/1password.list
+	sudo apt update && sudo apt install 1password
+
 	apt autoremove
 	apt autoclean
 	apt clean
