7add28ec55287c172207988f16aaa2073631cc3d
Lewis Denham-Parry <lewis@control-plane.io>
2022-02-07 17:24:42 +0000

    feat: update apt source
    - move from buster to bullseye

--- bin/install.sh
+++ bin/install.sh
@@ -76,44 +76,20 @@ setup_sources() {
 		--no-install-recommends
 
 	cat <<-EOF > /etc/apt/sources.list
-	deb http://httpredir.debian.org/debian buster main contrib non-free
-	deb-src http://httpredir.debian.org/debian/ buster main contrib non-free
+	deb http://deb.debian.org/debian/ bullseye main non-free contrib
+	deb-src http://deb.debian.org/debian/ bullseye main non-free contrib
 
-	deb http://httpredir.debian.org/debian/ buster-updates main contrib non-free
-	deb-src http://httpredir.debian.org/debian/ buster-updates main contrib non-free
+	deb http://security.debian.org/debian-security bullseye-security main contrib non-free
+	deb-src http://security.debian.org/debian-security bullseye-security main contrib non-free
 
-	deb http://security.debian.org/ buster/updates main contrib non-free
-	deb-src http://security.debian.org/ buster/updates main contrib non-free
-
-	deb http://httpredir.debian.org/debian experimental main contrib non-free
-	deb-src http://httpredir.debian.org/debian experimental main contrib non-free
+	deb http://deb.debian.org/debian/ bullseye-updates main non-free contrib
+	deb-src http://deb.debian.org/debian/ bullseye-updates main non-free contrib
 	EOF
 
 	# turn off translations, speed up apt update
 	mkdir -p /etc/apt/apt.conf.d
 	echo 'Acquire::Languages "none";' > /etc/apt/apt.conf.d/99translations
 
-	# Create an environment variable for the correct distribution
-	CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-	export CLOUD_SDK_REPO
-
-	# Add the Cloud SDK distribution URI as a package source
-	cat <<-EOF > /etc/apt/sources.list.d/google-cloud-sdk.list
-	deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main
-	EOF
-
-	# Import the Google Cloud Platform public key
-	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
-	# Add the Google Chrome distribution URI as a package source
-	cat <<-EOF > /etc/apt/sources.list.d/google-chrome.list
-	deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
-	EOF
-
-	# kubectl
-	cat <<-EOF > /etc/apt/sources.list.d/kubernetes.list
-	deb https://apt.kubernetes.io/ kubernetes-xenial main
-	EOF
 }
 
 base_min() {
