f85ced0439a7251f6c93268e3af4f492da5583c2
Lewis Denham-Parry <lewis@control-plane.io>
2022-02-07 17:28:29 +0000

    feat: setup base install
    - update apt install packages for base

--- bin/install.sh
+++ bin/install.sh
@@ -169,13 +169,11 @@ base() {
 		gettext \
 		git-crypt \
 		gnupg-agent \
-		google-cloud-sdk \
 		graphviz \
 		hplip \
 		iwd \
 		imagemagick \
 		kitty \
-		kubectl \
 		libapparmor-dev \
 		libimobiledevice6 \
 		libltdl-dev \
@@ -196,13 +194,6 @@ base() {
 		systemd \
 		tmux \
 		--no-install-recommends
-
-	setup_sudo
-
-	# 1password
-	sudo apt-key --keyring /usr/share/keyrings/1password.gpg adv --keyserver keyserver.ubuntu.com --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
-	echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password.gpg] https://onepassword.s3.amazonaws.com/linux/debian edge main' | sudo tee /etc/apt/sources.list.d/1password.list
-	sudo apt update && sudo apt install 1password
 
 	apt autoremove
 	apt autoclean
@@ -296,3 +287,7 @@ main() {
 }
 
 main "$@"
+
+
+# google-cloud-sdk
+# kubectl
