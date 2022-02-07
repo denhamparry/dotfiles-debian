8a096be9ba5707730df6cf062ba6e8499b7ecc96
Lewis Denham-Parry <lewis@control-plane.io>
2022-02-07 19:46:20 +0000

    fix: fix kubectl install scripts
    - move to tmp directory to install script

--- bin/install.sh
+++ bin/install.sh
@@ -232,11 +232,13 @@ install_scripts() {
 	fi
 
 	# kubectl
-	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" > /tmp/scripts/kubctl
-	curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256" > /tmp/scripts/kubctl.sha256
-	echo "$(</tmp/scripts/kubctl.sha256)  /tmp/scripts/kubctl" | sha256sum --check
-	sudo install -o root -g root -m 0755 /tmp/scripts/kubctl /usr/local/bin/kubectl
+	cd /tmp/scripts/
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+	echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
+	sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 	kubectl version --client
+	cd -
 
 	# starship
 	sh -c "$(curl -fsSL https://starship.rs/install.sh)"
