43e1a811828ffac8026b91faa947fd2d5ddeb9b1
Lewis Denham-Parry <lewis@control-plane.io>
2022-02-18 19:29:14 +0000

    fix: remove docker key removal
    - fails if file isn't already there

--- bin/install.sh
+++ bin/install.sh
@@ -85,7 +85,6 @@ setup_sources() {
 	EOF
 
 	# Docker
-	rm /usr/share/keyrings/docker-archive-keyring.gpg
 	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
  	echo \
 	"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
